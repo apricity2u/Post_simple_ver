27913b4bc2ace17107e877f86dfad00275329975
apricity2u <gkdms9849@gmail.com>
2025-02-10 02:34:03 +0900

    refactor: RootHeader - Link 추가 및 div 태그 nav 태그로 변경

--- frontend/my-app/src/component/RootHeader.jsx
+++ frontend/my-app/src/component/RootHeader.jsx
@@ -1,14 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function RootHeader() {
   return (
-    <div className='root-header'>
-        <img src="src/assets/와글와글_logo.png" alt="와글와글로고" className='logo'/>
+    <nav className="root-header">
+      <Link to="/">
+        <img
+          src="src/assets/와글와글_logo.png"
+          alt="와글와글로고"
+          className="logo"
+        />
+      </Link>
+      <Link to="/">
         <h1>와글와글</h1>
-        <div className='root-header-button-box'>
-            <button>마이페이지</button>
-            <button>로그인</button>
-        </div>
-    </div>
-  )
+      </Link>
+      <div className="root-header-button-box">
+        <Link to="/my-page">
+          <button>마이페이지</button>
+        </Link>
+        <Link to="/login">
+          <button>로그인</button>
+        </Link>
+      </div>
+    </nav>
+  );
 }
