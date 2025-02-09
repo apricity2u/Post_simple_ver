95412f7f9e0e58bdbf2694454c1168addaff932a
apricity2u <gkdms9849@gmail.com>
2025-02-10 03:21:59 +0900

    feat: 게시글 목록 렌더링 형태 잡기

--- frontend/my-app/src/pages/PostList.jsx
+++ frontend/my-app/src/pages/PostList.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
 
 export default function PostList() {
   return (
-    <div>PostList</div>
-  )
+    <div className="post-list">
+      <h2>게시글 목록</h2>
+      <ul>
+        <li>
+          <span>게시글 제목</span>
+          <span>작성시간</span>
+        </li>
+        <li>
+          <span>게시글 제목</span>
+          <span>작성시간</span>
+        </li>
+        <li>
+          <span>게시글 제목</span>
+          <span>작성시간</span>
+        </li>
+        <li>
+          <span>게시글 제목</span>
+          <span>작성시간</span>
+        </li>
+      </ul>
+    </div>
+  );
 }
