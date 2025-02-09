c381272896c1e57fda63c8fd5116dcc6b40feee3
apricity2u <gkdms9849@gmail.com>
2025-02-09 23:50:43 +0900

    docs: 와이어프레임 기반 페이지 생성 및 페이지별 router 설정

--- frontend/my-app/src/router/index.jsx
+++ frontend/my-app/src/router/index.jsx
@@ -1,18 +1,50 @@
 import { createBrowserRouter } from "react-router-dom";
 import Home from "../pages/Home";
 import RootLayout from "../layout/RootLayout";
+import Login from "../pages/Login";
+import Signup from "../pages/Signup";
+import PostList from "../pages/PostList";
+import PostDetail from "../pages/PostDetail";
+import MyPage from "../pages/MyPage";
+import NotFound from "../pages/NotFound";
+import PostCreate from "../pages/PostCreate";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <RootLayout />,
+    errorElement: <NotFound />,
     children: [
       {
         index: true,
         element: <Home />,
+      },
+      {
+        path: "/login",
+        element: <Login />
+      },
+      {
+        path: "/signup",
+        element: <Signup />
+      },
+      {
+        path: "/my-page",
+        element: <MyPage />
+      },
+      {
+        path: "/posts",
+        element: <PostList />
+      },
+      {
+        path: "/posts/create",
+        element: <PostCreate />
+      },
+      {
+        path: "/posts/:postId",
+        element: <PostDetail />
       }
     ],
-  },
+  }
 ]);
 
 export default router;
