265c715e5834357bc02281d50cd3a274fabc46b7
apricity2u <gkdms9849@gmail.com>
2025-02-10 02:26:24 +0900

    feat: Home과 PostList 연결 - Link 태그 사용

--- frontend/my-app/src/pages/Home.jsx
+++ frontend/my-app/src/pages/Home.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Home() {
   return (
+    <Link to='/posts'>
       <div className='text-bigger'>와글와글 떠들러 가기</div>
+    </Link>
   )
 }
